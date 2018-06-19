0d5ee43c2b8791d0b7b43205a7123747ea670373
Junyangz <junyangz.iie@gmail.com>
2018-06-19 09:46:41 +0800

    update LdbcQuery3Handler of janusgraph impl

--- janusgraph/src/main/java/com/ldbc/snb/janusgraph/drivers/interactive/LdbcQuery3Handler.java
+++ janusgraph/src/main/java/com/ldbc/snb/janusgraph/drivers/interactive/LdbcQuery3Handler.java
@@ -5,12 +5,15 @@ import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery3;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery3Result;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.directory.SchemaViolationException;
 import java.util.*;
 
+import static com.ldbc.snb.janusgraph.drivers.interactive.QueryUtils.CODE_OK;
 /**
  * Created by Tomer Sagi on 06-Oct-14.
  * Handles Q3 by running a groupBy pipe and then sorting using
@@ -23,115 +26,81 @@ public class LdbcQuery3Handler implements OperationHandler<LdbcQuery3,JanusGraph
     public void executeOperation(final LdbcQuery3 operation, JanusGraphDb.RemoteDBConnectionState dbConnectionState, ResultReporter
             resultReporter) throws DbException {
 
-/*        long person_id = operation.personId();
-        final String country_x = operation.countryXName();
-        final String country_y = operation.countryYName();
-        final long min_date = operation.startDate().getTime();
-        final long max_date = QueryUtils.addDays(min_date, operation.durationDays());
-        logger.debug("Query 3 called on {} for country x {} country y {} min_date {} max _date {}"
-                , person_id, country_x, country_y, min_date, max_date);
+        Map<String,Object> parameters = new HashMap<String,Object>();
+        parameters.put("$person_id", operation.personId());
+        parameters.put("$country_x", operation.countryXName());
+        parameters.put("$country_y", operation.countryYName());
+        parameters.put("$min_date", operation.startDate().getTime());
+        int days = operation.durationDays();
+        long max_date = operation.startDate().getTime() + days * 24 * 60 * 60 * 1000;
+        parameters.put("$max_date", max_date);
+        parameters.put("$limit", operation.limit());
 
-        int limit = operation.limit();
-        JanusGraphDb.BasicClient client = dbConnectionState.client();
+        String query = "g.V().has('Person.id', $person_id)" +
+                ".repeat(__.out('knows')).emit().times(2)" +
+                ".has('Person.id', P.neq($person_id))" +
+                ".dedup()" +
+                ".where(" +
+                "__.out('isLocatedIn')" +
+                ".out('isPartOf')" +
+                ".and(" +
+                "__.has('name', P.neq($country_x))," +
+                "__.has('name', P.neq($country_y))" +
+                ")" +
+                ")" +
+                ".as('person', 'xCount', 'yCount')" +
+                ".select('person', 'xCount', 'yCount')" +
+                ".by()" +
+                ".by(" +
+                "__.in('hasCreator')" +
+                ".where(" +
+                "__.and(" +
+                "__.has(('creationDate'), P.gte($min_date)), " +
+                "__.has(('creationDate'), P.lt($max_date))" +
+                ")" +
+                ")" +
+                ".out('isLocatedIn')" +
+                ".has('name', $country_x).count())" +
+                ".by(" +
+                "__.in('hasCreator')" +
+                ".where(" +
+                "__.and(" +
+                "__.has(('creationDate'), P.gte($min_date)), " +
+                "__.has(('creationDate'), P.lt($max_date))" +
+                ")" +
+                ")" +
+                ".out('isLocatedIn')" +
+                ".has('name', $country_y).count())" +
+                ".limit($limit)" +
+                ".order().by('xCount',Order.decr).by('Person.id', Order.incr)";
 
-        //collect cities to exclude friends with
-        Vertex country;
-        final Set<Long> cityIDs = new HashSet<>();
         try {
-            Map<String, String> pvMap = new HashMap<>();
-            pvMap.put("type", "country");
-            pvMap.put("name", country_x);
-            country = client.getVertices("Place", pvMap, Integer.MAX_VALUE).iterator().next();
-            cityIDs.addAll(QueryUtils.getCities(country));
-
-            pvMap.put("name", country_y);
-            country = client.getVertices("Place", pvMap, Integer.MAX_VALUE).iterator().next();
-            cityIDs.addAll(QueryUtils.getCities(country));
-
-        } catch (SchemaViolationException e) {
+            ResultSet resultSet = dbConnectionState.runQuery(query, parameters);
+            ArrayList<LdbcQuery3Result> results = new ArrayList<LdbcQuery3Result>();
+            for (Result r : resultSet) {
+                Map<String,List<Object>> map = (Map<String,List<Object>>)r.getObject();
+                Map<String,List<Object>> person = (Map<String,List<Object>>)map.get("person");
+//                long  xCount = (Long)((Map<String,List<Object>>)map.get("xCount").get(0);
+//                long  yCount = (Long)(Map<String,List<Object>>)map.get("yCount").get(0);
+                Map<String,List<Object>> xCountMap = (Map<String,List<Object>>)map.get("xCount");
+                Map<String,List<Object>> yCountMap = (Map<String,List<Object>>)map.get("yCount");
+                long xCount = (Long) xCountMap.get("xCount").get(0);
+                long yCount = (Long) yCountMap.get("yCount").get(0);
+                LdbcQuery3Result ldbcResult = new LdbcQuery3Result(
+                        (Long)person.get("Person.id").get(0),
+                        (String)person.get("Person.firstName").get(0),
+                        (String)person.get("Person.lastName").get(0),
+                        xCount,
+                        yCount,
+                        xCount+yCount);
+                results.add(ldbcResult);
+            }
+            resultReporter.report(CODE_OK, results, operation);
+        } catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
 
 
-        //Main Query
-        Map qResX = new HashMap<>();
-        Map qResY = new HashMap<>();
-        PipeFunction Q3KEY_FUNC = new PipesFunction<Vertex, Vertex>() {
-            @Override
-            public Vertex compute(Vertex argument) {
-                return (Vertex) this.asMap.get("friend");
-            }
-        };
-
-        GremlinPipeline<Collection<Vertex>, Vertex> gp = new GremlinPipeline<>(QueryUtils.getInstance().getFoF(person_id, client));
-        gp.filter(new PipeFunction<Vertex, Boolean>() { //Friends not from countries X and Y
-            public Boolean compute(Vertex v) {
-                Long friendCityID = (Long) (new GremlinPipeline<Vertex, List<Vertex>>(v)).as("root").out("isLocatedIn").toList().get(0).getId();
-                return !cityIDs.contains(friendCityID);
-            }
-        }).as("friend")
-                .in("hasCreator").interval("creationDate", min_date, max_date).as("post")
-                .out("isLocatedIn").has("name", country_x).groupCount(qResX, Q3KEY_FUNC)
-                .back("friend").in("hasCreator").interval("creationDate", min_date, max_date)
-                .out("isLocatedIn").has("name", country_y)
-                .groupCount(qResY, Q3KEY_FUNC).iterate();
-
-
-        //Collect results and summarize
-        Map<Vertex, Q3Res> counter = new HashMap<>();
-        for (Object o : qResX.keySet()) {
-            Vertex friendV = (Vertex) o;
-            if (!qResY.containsKey(o))
-                continue;
-
-            int xRes = ((Long) qResX.get(o)).intValue();
-            int yRes = ((Long) qResY.get(o)).intValue();
-            counter.put(friendV, new Q3Res(xRes, yRes));
-        }
-
-        //sort
-        counter = QueryUtils.sortByValueAndKey(counter, false, true, JanusGraphDb.BasicClient.IDCOMP);
-
-        //parse map to result
-        List<LdbcQuery3Result> result = new ArrayList<>();
-        int i = 1;
-        for (Map.Entry<Vertex, Q3Res> entry : counter.entrySet()) {
-            Vertex friend = entry.getKey();
-            long id = client.getVLocalId((Long) friend.getId());
-            i++;
-            LdbcQuery3Result res = new LdbcQuery3Result(id, (String) friend.getProperty("firstName")
-                    , (String) friend.getProperty("lastName"),
-                    entry.getValue().x, entry.getValue().y, entry.getValue().x + entry.getValue().y);
-            result.add(res);
-            if (i == limit)
-                break;
-        }
-
-        resultReporter.report(result.size(), result, operation);
-        */
     }
-
-    /*
-    protected class Q3Res implements Comparable<Q3Res> {
-        public int x = 0;
-        public int y = 0;
-
-        public Q3Res(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return this.getClass().isInstance(o) && (this.x == ((Q3Res) o).x && this.y == ((Q3Res) o).y);
-        }
-        @Override
-        public int compareTo(Q3Res o) {
-            if (this.getClass().isInstance(o)) {
-                return Integer.compare(x + y, o.x + o.y);
-            } else
-                return 0;
-        }
-    }
-    */
 }
